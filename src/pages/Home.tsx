06c0536ad0efb2fbeca42e73ecd85c489b2efdb0
jefta-jose <ndegwajeff4@gmail.com>
2025-01-03 14:43:30 +0300

    contact section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import AboutSection from "@/components/HomePageSections/AboutSection"
 import CatalogueSection from "@/components/HomePageSections/CatalogueSection"
 import CatalogueSwiperSection from "@/components/HomePageSections/CatalogueSwiperSection"
 import CompanySection from "@/components/HomePageSections/CompanySection"
+import ContactSection from "@/components/HomePageSections/ContactSection"
 import HeroSection from "@/components/HomePageSections/HeroSection"
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <AboutSection/>
       <CatalogueSection/>
       <CatalogueSwiperSection/>
+      <ContactSection/>
     </div>
   )
 }
